f52826431bee4028e11a1352b9e12dfa44ad5793
santoshborra <santosh.ece08@gmail.com>
2018-04-13 02:12:50 +0530

    New tasks

---
+++ Apr-13.sql
@@ -0,0 +1,11 @@
+
+1. How to drop a parent table without dropping the child table.
+
+2. What is a database system identifier and how to find it.
+
+3. How to find the query execution duration time from log file.
+
+4. What is the difference between TRUNCATE and DELETE statement. (Google)
+
+5. Demonstrate the usage of TRANSACTION REPEATABLE READ and SERIALIZATION.
+

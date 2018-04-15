a6c9aa5d710d122940c3a87d09d62a015f8e3edb
santoshborra <santosh.ece08@gmail.com>
2018-04-16 02:16:23 +0530

    updates
    new ans

---
+++ 11.sql
@@ -0,0 +1,13 @@
+ 1. Describe each character meaning in SELECT current_timestamp.
+
+
+ 2. Demonstrate the temporary file generation
+
+
+ 3. How to kill a postgres process manually (Google)
+
+
+ 4. Demonstrate the password less access between two machines (ssh)
+
+
+ 5. Demonstrate the usage of scp and rsync
